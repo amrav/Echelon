2c684b7bd43a8a4241cc0d0516a802c640b2a466
Vikrant <ubuntu@ubuntu-VirtualBox.(none)>
2011-12-31 07:43:07 +0530

    Added statement class, and cleaned up parsing using re.

--- parse_input.py
+++ parse_input.py
@@ -3,7 +3,9 @@ import nltk
 import re
 
 global chatlog_nltk 
-global chatlog_words
+global chatlog_words 
+global users
+global statements
 #the tokenized chatlog
 
 def clean (text):
@@ -17,11 +19,21 @@ def clean (text):
                 else:
                     cleantext_str+=' '
     #text = text.strip('\n') Why isn't this working instead of the above?
+    matches = re.findall(r"\[([0-9][0-9]\:[0-9][0-9])\] <(.*?)> (.*)",text)
+    statements = []
+    users = []
+    for match in matches:
+        #times.append(match[0])
+        users.append(match[1])
+        stat = statement(match[0],match[1],match[2]) 
+        statements.append(stat)
+    for stat in statements:
+        print stat.text
     return cleantext_str.split(' ')
-    
+   
 
-def tokenize (filename):
-    '''tokenize the text at filename''' 
+def init (filename):
+    '''tokenize the text at filename and build various global lists''' 
     #print filename
     text = open(filename,'rU').read()
     #print type(text)
@@ -33,21 +45,34 @@ def tokenize (filename):
     #print type(tokens)
     #print type(chatlog)
     #print chatlog_nltk.concordance('He')
+    #build_user_list_special(chatlog_words)
 
-#def build_user_list_special (chatlog_words) :
- #   for i in range(len(chatlog_words-10)):
-  #      if chatlog_words[i] == '*':
-   #         if chatlog_words[i+1] == '*' and chatlog_words[i+2] == '*':
-                
+def build_user_list_special(words):
+    users = []
+    for i in range(len(words)):
+        if words[i] == '***' and words[i+2] == 'joined':  
+            if words[i+1] not in users:
+                users.append(words[i+1])
+            i+=3
+    #for user in user_list:
+     #   print user
         
+#def build_statement_list(words):
+    
+
+class statement:
+    def __init__(self, time, issuing_user_name, statement_text):
+        self.text = statement_text
+        self.issued_by = issuing_user_name
+        self.time = time
+
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print "Usage: python parse_input filename"
-        #sys.exit(1)
-    else:
-        filename = sys.argv[1]
+        sys.exit(1)
+    filename = sys.argv[1]
+    init(filename)
     #tokenize(filename)
 
-    
 
