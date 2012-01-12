8814d67e67ad9a1a8e9d7614e3b34d9caefc32ba
rishi <rishicomplex@gmail.com>
2012-01-13 02:00:44 +0530

    Added success percentage check to test_algorithm.py
    Signed-off-by: Vikrant Varma <vikrant.varma94@gmail.com>
    
    tested test_algorithm.py with answer correctness detection, working fine
    
    Signed-off-by: Vikrant Varma <vikrant.varma94@gmail.com>
    
    added .gitignore
    
    Signed-off-by: Vikrant Varma <vikrant.varma94@gmail.com>

--- test_algorithm.py
+++ test_algorithm.py
@@ -10,10 +10,14 @@ if __name__ == '__main__':
         sys.exit(1)
     from settings import test_alg
     statements = parse_input.init(sys.argv[1])
+    answers_file=open(sys.argv[1][:-8]+'answers.txt','r') #opening the corresponding _answers.txt file 
+    deleted_nicks=0 #counts how many $$$s were there
+    correct_answers=0 #counts how many the algorithm got correct
     test_alg.run(statements)
     for i in range(len(statements)):
         if statements[i].issued_by == '$$$':
             print '-------------------------'*3
+            deleted_nicks+=1
             print test_alg 
             print '-------------------------'*3
             print
@@ -28,6 +32,13 @@ and user!='$$$':
                     answer = user
                     break
             print "Replacing $$$ by", answer
+            correct_answer=answers_file.readline()[:-1]
+            if correct_answer==answer:
+                correct_answers+=1
+                print "Answer is correct."
+            else :
+                print "Answer is wrong. Correct answer was "+correct_answer
             print stat.alg_lambda
             print
-    
+    print "Success percentage :",float(correct_answers)*100/deleted_nicks
+    print correct_answers,"out of",deleted_nicks
