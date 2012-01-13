7b0eb0338665f25dd7a903e0d2fce455a997f74a
Vikrant Varma <vikrant.varma94@gmail.com>
2012-01-14 02:32:50 +0530

    Add time_diff to alg_bracket.py

--- algorithms/alg_bracket.py
+++ algorithms/alg_bracket.py
@@ -1,8 +1,10 @@
+from __future__ import division
 import re
 
 stat_scope = 1 #number of statements after the unknown statement which the algorithm will look at
 text_scope = 0 #maximum number of letters after which bracket can start
-user_lambda = 200000 #lambda this algorithm will assign
+user_lambda = 60 #lambda this algorithm will assign
+time_diff = 1.5 #roughly, the number of minutes after which the bracket can appear
 
 def run(statements):
     for i in range(len(statements)):
@@ -10,6 +12,7 @@ def run(statements):
         stat.alg_lambda = {}
         if stat.issued_by == '$$$':
             for st in statements[i+1:i+1+stat_scope]:
+                diff = (st.total_time - stat.total_time)/60
                 match = re.search(r'^.{0,'+str(text_scope)+r'}\(.*?\)',' '.join(st.text))
-                if match != None:
+                if match != None and diff<time_diff:
                     stat.alg_lambda[st.issued_by] = user_lambda
