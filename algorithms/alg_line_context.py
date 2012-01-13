bf22cdf0bb160055876dfc22c9add8f99120d001
Vikrant Varma <vikrant.varma94@gmail.com>
2012-01-14 03:31:00 +0530

    Add optimise.py to optimise algorithms.
    Make optimise work.
    
    Create module optimise:
    
           * Implement testing an algorithm by simply specifying an algorithm on settings.py and chatlog on the command line.
           * Improve create_test_log and parse_input to operate in RAM rather than from file.
    
    Optimisation algorithm finally working.
    
    	     * Implement tools to understand the data
    	     * Write docs
    	     * Modify existing alg_line_context to run it through optimise.py
    
    Bring optimise.py to a manageable shape. Needs testing.

--- algorithms/alg_line_context.py
+++ algorithms/alg_line_context.py
@@ -1,80 +1,85 @@
 from __future__ import division
+from math import log
 import sys
 import re
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
+##print sys.path
 import parse_input
 
+params = []
+ranges = []
+steps = []
 
-current_scope = 10
-scope_weight = 4
-scope_weight_minus = 5
-addressed_weight = 4
-inverse_recent_weight = 1
+
+params += [5]  #issue_scope
+ranges += [(1,10)]
+steps += [1]
+
+params += [5] #scope_weight
+ranges +=  [(1,10)]
+steps += [1]
+
+params += [3] #scope_weight_minus
+ranges += [(0,10)]
+steps += [1]
 
 class context_statement(parse_input.statement):
-    def init(self):
-        self.current_users = {}
-    def add_current_user(self, user,scope=0):
-        self.current_users[user] = scope
-    def delete_current_user(self, user):
-        self.current_users.pop(user)
+    scope = {}
+    #for debugging:
+    def set_curr_users(self, curr_users):
+        self.curr_users = curr_users
+    def print_details(self):
+        parse_input.statement.print_details(self)
+        print "Current Users: ", self.curr_users
+        if self.alg_lambda != {}:
+            print "Algorithm lambdas: ", self.alg_lambda
+        print 
+        print
+        print
 
 def run_alg(statements):
+    #inititalise parameters
+    issue_scope = params[0]
+    scope_weight = params[1]
+    scope_weight_minus = params[2]
 #function that runs the line context algorithm      
-    
     for stat in statements:
-        stat.init()
-        text = ''
-        for word in stat.text:
-            text+=word
-        address = re.match(r"(.*?):", text)
-        if address != None:
-            addressed_user = address.group(1)
-            if addressed_user in parse_input.statement.users:
-                stat.current_users[addressed_user] = \
-current_scope*addressed_weight
+        #assign user_lambdas if required:
+        if stat.issued_by == '$$$':
+            stat.alg_lambda = {} 
+            if len(context_statement.scope) == 0: #if nothing is in scope, no guesses may be made
+                continue
+            else:
+                ##print context_statement.scope.values()
+                avg = sum([log(value) for value in context_statement.scope.values()])
+                avg /= len(context_statement.scope)
+            ##print avg
 
-    #update current_users list
-        if stat.issued_by not in stat.current_users:
-            stat.add_current_user(stat.issued_by, current_scope*scope_weight)
-        for user in stat.current_users:
-            stat.current_users[user]-=1*scope_weight_minus
-            if stat.current_users[user]<0:
-                stat.delete_current_user(user)
-            
-    for i in range(len(statements)):
-        if statements[i].issued_by == '$$$':
-            cumulative_scope = {}
-            for stat in statements[(i-current_scope):i+1]:
-                for user in stat.current_users:
-                    scope = stat.current_users[user]
-                    if user not in cumulative_scope:
-                        cumulative_scope[user]=scope
-                    else:
-                        cumulative_scope[user]+=scope
-            for user in cumulative_scope:
-                if user in statements[i].current_users:
-                    #This function needs to be refined. Perhaps exponential?
-                    cumulative_scope[user]+=\
-int(statements[i].current_users[user]\
-/(scope_weight*inverse_recent_weight))#/scope_weight
-            max_scope = 0
-            for user in cumulative_scope:
-                if cumulative_scope[user]>max_scope and user!='$$$'\
-and (user+':') not in stat.text:
-                    answer = user
-                    max_scope = cumulative_scope[user]
-            for user in cumulative_scope:
-                statements[i].alg_lambda = cumulative_scope 
-    for stat in statements:
-        if stat.issued_by == '$$$' and stat.alg_lambda!=None and stat.alg_lambda!={}:
-            avg = sum(stat.alg_lambda.values())/len(stat.alg_lambda.keys())
-            #Divide by average to normalise values around 1 ?
-            for user in stat.alg_lambda:
-                stat.alg_lambda[user]/=avg
-                #If user is referred to, his lamda becomes zero.
-                if user+':' in stat.text:
-                    stat.alg_lambda[user] = 0
-    return statements    
+            for user in context_statement.scope: #Normalise the scope scores by dividing by the average
+                if avg != 0:
+                    stat.alg_lambda[user] = log(context_statement.scope[user])/avg
+                ##print stat.alg_lambda[user],
+            ##print
+        #Otherwise update scope
+        elif stat.issued_by not in context_statement.scope:
+            context_statement.scope[stat.issued_by] = scope_weight*issue_scope
+        else:
+            context_statement.scope[stat.issued_by] += scope_weight
+        
+        #list of users fallling out of scope
+        popped_users = []
+        for user in context_statement.scope:
+            context_statement.scope[user] -= scope_weight_minus
+            if context_statement.scope[user]<=0:
+                popped_users.append(user)
+        for user in popped_users:
+            context_statement.scope.pop(user)
+        #for debugging
+        stat.curr_users = context_statement.scope.items()
+        
+        
+    
 
 def run(statements):
     #Populates alg_lamda in each statement
@@ -88,9 +93,7 @@ if __name__ == '__main__':
         print "Usage: python alg_line_context.py filename"
         sys.exit(1)
     statements = parse_input.init(sys.argv[1])
+    run(statements)
     for stat in statements:
-        stat.__class__ = context_statement
-    run_alg(statements)
-    for stat in statements:
-        if stat.issued_by == '$$$':
-            stat.print_details()
+        ##if stat.issued_by == '$$$':
+        stat.print_details()
