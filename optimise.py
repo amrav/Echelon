0240c629a8c95cfb45671fe0abac1332e9090e50
Vikrant Varma <vikrant.varma94@gmail.com>
2012-01-14 03:31:00 +0530

    Add optimise.py to optimise algorithms.
    Make optimise work.
    
    Create module optimise:
    
           * Implement testing an algorithm by simply specifying an algorithm on settings.py and chatlog on the command line.
           * Improve create_test_log and parse_input to operate in RAM rather than from file.
    
    Optimisation algorithm finally working.
    
    	     * Implement tools to understand the data
    	     * Write docs
    	     * Modify existing alg_line_context to run it through optimise.py
    
    Bring optimise.py to a manageable shape. Needs testing.

--- optimise.py
+++ optimise.py
@@ -3,8 +3,9 @@ import sys
 import parse_input
 from create_test_log import createlog
 from settings import opt_alg
+from settings import opt_runs
 
-runs = 5
+runs = opt_runs
 
 def nonint_range(lower, upper, step):
     i = lower
@@ -26,6 +27,7 @@ def vary(ranges, param_steps):
                 perms.append(perm + [i])
         return perms
         
+<<<<<<< HEAD
 def findmax(testfile_text, answerfile_text, alg):
     stats = {}
     
@@ -41,11 +43,47 @@ def findmax(testfile_text, answerfile_text, alg):
         for i in range(runs):
             texts = createlog(filetext = logtext) #returns a list of the test text and the answer text
             ##print texts[0]
+=======
+def findmax(filename, alg, printmax = True, result_filename = ''):
+    ##print 'FINDMAX!'
+    stats = {}
+    count = 1
+    all_stats = {}
+    print 'Building parameter permutations...',
+    param_perms = vary(alg.ranges, alg.steps)
+    print 'Done.'
+    print 'Loading chatlog...',
+    logtextfile = open(filename)
+    logtext = logtextfile.read()
+    ##print logtext
+    logtextfile.close()
+    print 'Done.'
+    print 'Looping through parameter permutations. '
+    for param_perm_num in range(len(param_perms)):
+        param_perm = param_perms[param_perm_num]
+        print '\rTesting with parameters', param_perm, '.........', int((param_perm_num+1)*100/len(param_perms)), '%'
+        
+        stats['correct']=stats['unanswered']=stats['total']=0
+        if printmax != True:
+            print
+            print
+            print '----------'*4
+            print 'Run', count, ': Testing with parameters,', param_perm
+            print '----------'*4
+        
+        alg.params = param_perm
+        
+        for i in range(runs):
+            texts = createlog(filetext = logtext) #returns a list of the test text and the answer text
+            ##print 'Created logfile'
+            ##print texts[1]
+>>>>>>> 3348849... Create module optimise:
             ##print 'End print'
             run_stats = check(texts[0], texts[1], alg)
             stats['correct'] += run_stats['correct']
             stats['unanswered'] += run_stats['unanswered']
             stats['total'] += run_stats['total']
+<<<<<<< HEAD
             i += 1
             
         stats['correct'] *= 100 / stats['total']
@@ -58,6 +96,56 @@ stats['correct']*100/(100-stats['unanswered']), '% success.'
         
 
 
+=======
+            
+            if (i+1) % 13 == 0:
+                sys.stdout.write('\rRun {0} of {1}.........{2}%'.format((i+1), runs, int((i+1)*100/runs)))
+                sys.stdout.flush()
+            i += 1
+        sys.stdout.write('\r                                              \r{0} runs done.'.format(runs))
+        sys.stdout.flush()
+        print
+        if stats['total'] != 0:
+            stats['correct'] *= 100 / stats['total']
+            stats['unanswered'] *= 100 / stats['total']
+        
+            if stats['unanswered'] != 100 and stats['correct'] not in all_stats:
+                all_stats[str(param_perm)] = str(stats['correct']*100/(100-stats['unanswered'])) + ' % success, ' + str(stats['correct']) + ' % correct, ' + str(100-stats['correct']-stats['unanswered']) + \
+' % wrong, ' + str(stats['unanswered']) + ' % unanswered, ' + str( stats['total']) + ' total. ' + \
+'\nRun ' + str(count) + ': Testing with parameters, ' + str(param_perm) + '\n'
+        
+            ##print all_stats
+        
+        ##print all_stats[stats['correct']]
+
+        if printmax != True:
+            if stats['unanswered'] == 100:
+                print 'None attempted.'
+            else:
+                print stats['correct'], '% correct,', 100-stats['correct']-stats['unanswered'], '% wrong,', stats['unanswered'], '% unanswered,', stats['total'], 'total,', \
+stats['correct']*100/(100-stats['unanswered']), '% success.'
+        count += 1
+    
+    print
+    if printmax == True and result_filename != '':
+        result = open(result_filename, 'w')
+        result.write('''-----------------------------------------------------------------------
+        
+        
+RESULTS
+        
+        
+-----------------------------------------------------------------------\n\n''')
+        result.write("Testing the file " + filename + '\n')
+        result.write("Algorithm: " + str(alg)+'\n')
+        result.write("Ranges: " + str(alg.ranges) + '\n')
+        result.write("Steps: " + str(alg.steps) + '\n\n')
+        
+        for stat in sorted(all_stats, key=lambda x: float(all_stats[x][:3]), reverse = True):
+            
+            result.write(all_stats[stat]+'\n')
+        result.close()
+>>>>>>> 3348849... Create module optimise:
 
 def check(testfile_text, answerfile_text, alg):
     stats = {} #answered, unanswered, total
@@ -86,14 +174,18 @@ def check(testfile_text, answerfile_text, alg):
                 stats['correct'] += 1
             ##stat.print_details()
             ##statements[i+1].print_details()
+<<<<<<< HEAD
+=======
 
     
+>>>>>>> 3348849... Create module optimise:
     return stats
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print 'Usage: python optimise.py filename'
+    if len(sys.argv) < 2:
+        print 'Usage: python optimise.py filename [resultfilename]'
         sys.exit(1)
+<<<<<<< HEAD
     stats = {} #for storing answered, unanaswered and total
     stats['correct']=stats['unanswered']=stats['total']=0
     logtextfile = open(sys.argv[1])
@@ -118,5 +210,11 @@ if __name__ == '__main__':
         print stats['correct'], '% correct,', 100-stats['correct']-stats['unanswered'], '% wrong,', stats['unanswered'], '% unanswered,', stats['total'], 'total,', \
 stats['correct']*100/(100-stats['unanswered']), '% success.'
 
+=======
+    if len(sys.argv) == 3:
+        findmax(sys.argv[1], opt_alg, printmax = True, result_filename = sys.argv[2])
+    else:
+        findmax(sys.argv[1], opt_alg, printmax = True)
+>>>>>>> 3348849... Create module optimise:
 
         
--- settings.py
+++ settings.py
@@ -6,5 +6,5 @@ import alg_bracket
 
 alg_list = [alg_line_context, alg_bracket]
 test_alg = alg_line_context
-opt_alg = alg_bracket
-
+opt_alg = alg_line_context
+opt_runs = 500
