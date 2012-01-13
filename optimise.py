172d238b39859d4254ccae103a8d90a41987cef3
Vikrant Varma <vikrant.varma94@gmail.com>
2012-01-14 03:31:00 +0530

    Add optimise.py to optimise algorithms.

---
+++ optimise.py
@@ -0,0 +1,33 @@
+import sys
+import parse_input
+from create_test_log import createlog
+from settings import opt_alg
+
+def check(testfile, answerfile, alg):
+    stats = {} #stats[0] = answered, stats[1] = unanswered, stats[2] = total
+    stats[0] = stats[1] = stats[2] = 0
+    statements = parse_input.init(testfile)
+    for stat in statements:
+        stat.alg_lambda = {}
+    alg.run(statements)
+    answers = open(answerfile, 'r')
+    for stat in statements:
+        if stat.issued_by == '$$$':
+            stats[2] += 1
+            answer = answers.readline()[:-1] #to remove the \n 
+            if stat.alg_lambda == {}:
+                stats[1] += 1
+            elif answer == stat.alg_lambda[sorted(stat.alg_lambda, key=stat.alg_lambda.get, reverse = True)[-1]]: #if answer == max value in alg_lambda
+                stats[0] += 1
+    return stats
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print 'Usage: python optimise.py filename'
+        sys.exit(1)
+    lognames = createlog(sys.argv[1], answerflag = True)
+    stats = check(lognames[0], lognames[1], opt_alg)
+    print stats
+    print type(stats)
+    print stats[0], 'correct,', stats[1], 'unanswered,', stats[2], 'total.'
+    
--- settings.py
+++ settings.py
@@ -6,3 +6,4 @@ import alg_bracket
 
 alg_list = [alg_line_context, alg_bracket]
 test_alg = alg_bracket
+opt_alg = alg_bracket
