996e47bbaa8540a952524cab12032cbe463aadbf
Vikrant Varma <vikrant.varma94@gmail.com>
2012-01-18 15:31:12 +0530

    Renamed alg_line... and alg_brac... to line... and brac... in algorithms/

--- settings.py
+++ settings.py
@@ -1,10 +1,10 @@
 import sys
 sys.path.append("./algorithms/")
 
-import alg_line_context
-import alg_bracket
+import line_context
+import bracket
 
-alg_list = [alg_line_context, alg_bracket]
-test_alg = alg_bracket
-opt_alg = alg_bracket
+alg_list = [line_context, bracket]
+test_alg = bracket
+opt_alg = bracket
 opt_runs = 30
