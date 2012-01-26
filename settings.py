27f04228c2f9a155cb9af1cc458a2ad0b4163174
Vikrant Varma <vikrant.varma94@gmail.com>
2012-01-27 03:44:54 +0530

    Use an emoticon text file to build an emoticon list, as implemented in the build_emoticons function.

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ import emot
 prev_display_scope = 4
 next_display_scope = 4
 
-alg_list = [line_context, bracket, addressal, user_words, typo]
+alg_list = [line_context, bracket, addressal, user_words, typo, emot]
 test_alg = emot
 opt_alg = typo
 opt_runs = 50
