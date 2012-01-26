cd1f4e092d650443af97c97f47bb46e1b8c10072
Vikrant Varma <vikrant.varma94@gmail.com>
2012-01-27 02:41:49 +0530

    Use stat.text_str to do a re search instead of stat.text.

--- algorithms/emot.py
+++ algorithms/emot.py
@@ -43,38 +43,45 @@ def emoticons_search (statements, downlimit, uplimit):
 
   for i in range(len(statements)):
     if statements[i].issued_by == '$$$':
+      #initialise curr_users
       curr_users = {}
+      #make sure that we are not very near the beginning or the end of the file
       if i> uplimit and i < len(statements)- downlimit :
+        #start checking for emoticons in the specified range
         for j in range(i-uplimit , i+downlimit+1 ):       
           stat = statements[j]
+          #don't using statements issued by unknown users
           if stat.issued_by != '$$$':
+            #initialise if necessary
             if stat.issued_by not in curr_users:
-              curr_users[stat.issued_by] = user(stat.issued_by)
+              curr_users[stat.issued_by] = user()
+            #increment the number of statements made by that user
             curr_users[stat.issued_by].statement_number += 1
-            for sta in stat.text:
-              for emoticon in emoticons:
-                ma = re.findall(emoticon,sta)
-                if ma != []:
-                  curr_users[stat.issued_by].emo[emoticon] += len(ma)
+            #search for any emoticons in the statement text
+            for emoticon in emoticons:
+              ma = re.findall(emoticon,stat.text_str)
+              #if any are found, increment emo for that user for that emoticon
+              if ma:
+                curr_users[stat.issued_by].emo[emoticon] += len(ma)
+      
       for users in curr_users:
         for emot in curr_users[users].emo:
-     
+          #normalise emo for users by dividing by number of statements made by users 
           curr_users[users].emo[emot]/= curr_users[users].statement_number
         
 
-
-      for sta in statements[i].text:
-        for emoticon in emoticons:
-          m= re.findall(emoticon,sta)
-          if m!=[]:
-            for usr in curr_users: #not users online it should be current users from line_context
+    
+      for emoticon in emoticons:
+        m = re.findall(emoticon,stat.text_str)
+        if m:
+          for usr in curr_users: #not users online it should be current users from line_context
               #print usr , curr_users[usr].emo[emoticon]
               #if curr_users[usr].emo[emoticon] > 0:
-              if curr_users[usr].emo[emoticon] != 0:
-                statements[i].alg_lambda[usr] = 1 + curr_users[usr].emo[emoticon]/10   # use this 10 as parameter to va
-              
-                
-      
+            if curr_users[usr].emo[emoticon] != 0:
+              statements[i].alg_lambda[usr] = 1 + curr_users[usr].emo[emoticon]/10   # use this 10 as parameter to va'''
+
+            
+            
 
 ##def print_details():
   #for users in curr_users:
